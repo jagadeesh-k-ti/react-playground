b09a2bdff1b07b7dcf5fb97aae12f6158f43a151
Jagadeesh Kotra <jagadeesh.kotra@technoidentity.com>
2023-06-02 23:23:46 +0530

    feat: PWA

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Playground
+
+### Generate PWA assets
+
+`pnpm pwa-asset-generator  public/logo.svg ./public -i index.html`
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,52 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true,
+  },
+  manifest: {
+    name: "React Playground",
+    short_name: "Playground",
+    description: "An experimental React + Vite project serving as scratchpad.",
+    icons: [
+      {
+        src: "manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "manifest-icon-192.maskable.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "maskable",
+      },
+      {
+        src: "manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any",
+      },
+      {
+        src: "manifest-icon-512.maskable.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "maskable",
+      },
+    ],
+    theme_color: "#2472d1",
+    background_color: "#071e3b",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
