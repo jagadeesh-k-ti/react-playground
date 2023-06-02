c60bbc0038acfb4cb796f48b1fdfb7a16804cfd6
Jagadeesh Kotra <jagadeesh.kotra@technoidentity.com>
2023-06-02 23:24:28 +0530

    feat: Counter Component + `tailwind` dark mode.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Counter } from "./components/Counter";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export default App;
